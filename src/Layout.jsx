cba408ce1016059ebb1e62e8d60b01c8c74905e2
Hetu289 <hetvimakasana289@gmail.com>
2026-04-09 23:06:15 +0530

    new data

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,8 +3,8 @@ import HeaderApp from './components/HeaderApp'
 import BannerApp from './components/BannerApp'
 import ContentApp from './components/ContentApp'
 import FooterApp from './components/FooterApp'
-import LoginApp from './components/pages/LoginApp'
-import CartApp from './components/pages/CartApp'
+import LoginApp from './components/pages/LoginApp.jsx'
+import CartApp from './components/pages/CartApp.jsx'
 export default function Layout() {
   return (
     <>
